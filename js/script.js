174272ddf96e37a39e746e247cb32c312a86feac
王宁 <m15944067543>
2025-06-03 18:16:30 +0800

    Site updated: 2025-06-03 18:16:29

--- js/script.js
+++ js/script.js
@@ -1,52 +1,67 @@
-(function($){
+// declaraction of document.ready() function.
+(function () {
+    var ie = !!(window.attachEvent && !window.opera);
+    var wk = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
+    var fn = [];
+    var run = function () {
+        for (var i = 0; i < fn.length; i++) fn[i]();
+    };
+    var d = document;
+    d.ready = function (f) {
+        if (!ie && !wk && d.addEventListener)
+            return d.addEventListener('DOMContentLoaded', f, false);
+        if (fn.push(f) > 1) return;
+        if (ie)
+            (function () {
+                try {
+                    d.documentElement.doScroll('left');
+                    run();
+                } catch (err) {
+                    setTimeout(arguments.callee, 0);
+                }
+            })();
+        else if (wk)
+            var t = setInterval(function () {
+                if (/^(loaded|complete)$/.test(d.readyState))
+                    clearInterval(t), run();
+            }, 0);
+    };
+})();
 
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
 
-      var alt = this.alt;
+document.ready(
+    // toggleTheme function.
+    // this script shouldn't be changed.
+    () => {
+        const pagebody = document.getElementsByTagName('body')[0]
 
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+        const default_theme = 'light' // 'dark'
 
-      $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
-    });
+        function setTheme(status = 'light') {
+            if (status === 'dark') {
+                window.sessionStorage.theme = 'dark'
+                pagebody.classList.add('dark-theme');
+                document.getElementById("switch_default").checked = true
+                document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+            } else {
+                window.sessionStorage.theme = 'light'
+                pagebody.classList.remove('dark-theme');
+                document.getElementById("switch_default").checked = false
+                document.getElementById("mobile-toggle-theme").innerText = "· Light"
+            }
+        };
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
+        setTheme(window.sessionStorage.theme ?? default_theme)
 
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
-
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
-})(jQuery);
+        document.getElementsByClassName('toggleBtn')[0].addEventListener('click', () => {
+            window.sessionStorage.theme = window.sessionStorage.theme === 'dark' ? 'light' : 'dark'
+            setTheme(window.sessionStorage.theme)
+            document.getElementById("switch_default").checked = window.sessionStorage.theme === 'light'
+        })
+        document.getElementById('mobile-toggle-theme').addEventListener('click', () => {
+            window.sessionStorage.theme = window.sessionStorage.theme === 'dark' ? 'light' : 'dark'
+            setTheme(window.sessionStorage.theme)
+            document.getElementById("mobile-toggle-theme").innerText = window.sessionStorage.theme === 'light' ? "· Light" : "· Dark"
+        })
+    }
+);
